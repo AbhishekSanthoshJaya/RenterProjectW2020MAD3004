6ed1148ef7b0acf692e963e7cb37b313a49f2157
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-12 18:09:18 -0500

    Initial Commit

---
+++ RenterProjectW2020MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
