386fc2303d1b3c4532d6e028b7e67e870501e344
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-12 18:46:41 -0500

    Added Vehicle class with proper initialized attributes

---
+++ RenterProjectW2020MAD3004/Vehicle.swift
@@ -0,0 +1,36 @@
+//
+//  Vehicle.swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+class Vehicle {
+    internal init(vehicleId: String, description: String?, manufacturer: String, isSelfDrive: Bool, isInsured: Bool, insuranceProviderName: String?, numberOfSeat: Int, baseRate: Float, perKmRate: Float) {
+        self.vehicleId = vehicleId
+        self.description = description
+        self.manufacturer = manufacturer
+        self.isSelfDrive = isSelfDrive
+        self.isInsured = isInsured
+        self.insuranceProviderName = insuranceProviderName
+        self.numberOfSeat = numberOfSeat
+        self.baseRate = baseRate
+        self.perKmRate = perKmRate
+    }
+    
+    private var vehicleId: String
+    private var description: String?
+    private var manufacturer:String
+    private var isSelfDrive: Bool
+    private var isInsured: Bool
+    private var insuranceProviderName: String?
+    private var numberOfSeat: Int
+    //private fuelType: FuelType
+    private var baseRate: Float
+    private var perKmRate: Float
+    
+    
+}
