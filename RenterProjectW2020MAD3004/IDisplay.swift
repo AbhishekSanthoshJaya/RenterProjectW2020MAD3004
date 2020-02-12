344624fbb486553e38efc60fdb86ebfc256fd290
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-12 18:26:36 -0500

    Added protocol

---
+++ RenterProjectW2020MAD3004/IDisplay.swift
@@ -0,0 +1,16 @@
+//
+//  IDisplay.swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+/**
+ * Protocol containing one method to  display()
+ */
+
+protocol IDisplayDelegate {
+     func display();
+}
