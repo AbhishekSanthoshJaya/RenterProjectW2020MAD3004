a4ac40694365a25982cb85e1de55b592eb47b3ac
raghavbobal1 <Shivji7&>
2020-02-12 22:19:22 -0500

    [Raghav Bobal] Created a new file Contact and a new class Contact in it with mobile no. , emailId and address variables

---
+++ RenterProjectW2020MAD3004/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+
+public class Contact {
+
+    private var mobileNumber: String
+    private var emailId: String
+    //private var address: Address
+}
