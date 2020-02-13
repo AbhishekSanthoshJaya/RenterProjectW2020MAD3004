f006ee537fbf426ff54ea3bf6e570feed50f4b0e
raghavbobal1 <Shivji7&>
2020-02-12 22:23:42 -0500

    [Raghav Bobal] Created an address with some variables

---
+++ RenterProjectW2020MAD3004/Address .swift
@@ -0,0 +1,17 @@
+//
+//  Address .swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+public class Address
+{
+    private var country: String
+    private var province: String
+    private var city: String
+   
+}
