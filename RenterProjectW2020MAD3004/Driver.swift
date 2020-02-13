af92a981ae43fd2f4b36da716bfb5d4593d524cb
raghavbobal1 <Shivji7&>
2020-02-12 22:52:19 -0500

    [Raghav Bobal]  Added a driver file with a driver class with some variables in it

---
+++ RenterProjectW2020MAD3004/Driver.swift
@@ -0,0 +1,16 @@
+//
+//  Driver.swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+public class Driver : Person
+{
+    private var drivingLicenceNumber: String
+    private var isDrivingHistoryClean: String
+    private var salary: Float
+}
