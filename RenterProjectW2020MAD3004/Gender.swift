596b28e22a57eab5e9a50198d5e24c4cc6b93494
raghavbobal1 <Shivji7&>
2020-02-12 20:39:06 -0500

    [Raghav Bobal] Created and enum class Gender

---
+++ RenterProjectW2020MAD3004/Gender.swift
@@ -0,0 +1,16 @@
+//
+//  Gender.swift
+//  RenterProjectW2020MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-12.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+public enum Gender
+{
+case MALE,
+     FEMALE,
+     OTHER
+}
