ff94f08ae2de5ced158a7b9629904ed000241eb7
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-19 17:42:21 -0500

    [Abhishek]Edited customer to conform to protocol Person

--- RenterProjectW2020MAD3004/Person/Customer.swift
+++ RenterProjectW2020MAD3004/Person/Customer.swift
@@ -10,11 +10,41 @@ import Foundation
 
 class Customer : Person
 {
+    var id: String
+    var firstName: String
+    var lastName: String
+    var gender: Gender.Type
+    var birthDate: Date?
+    var age: Int
+    var userName: String
+    var password: String
+    var contact: Contact
+    var address: Address
     private lazy var vehicleRents = [String: Vehicle]()
 
-    override init(id: String, firstName: String, lastName: String, gender: Gender.Type,birthDate: Date?, age: Int, userName: String, password: String, contact: Contact, address : Address)
- {
-    super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, age: age, userName: userName, password: password, contact: contact, address : address)
- }
+    required init(id: String, firstName: String, lastName: String, gender: Gender.Type,birthDate: Date?, age: Int, userName: String, password: String, contact: Contact, address : Address)
+    {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.birthDate = birthDate
+        self.age = age
+        self.userName = userName
+        self.password = password
+        self.contact = contact
+        self.address = address
+    }
+    
+    func display() {
+        print("\nID           : \(id)")
+        print("First Name     : \(firstName)")
+        print("Last Name      : \(lastName)")
+        print("Gender         : \(gender)")
+        print("Date of Birth  : \(birthDate ?? Date())")
+        print("Age            : \(age)")
+        print("Username       : \(userName)")
+
+    }
     //ToDo add a dictionary for the vehicle rent
 }
