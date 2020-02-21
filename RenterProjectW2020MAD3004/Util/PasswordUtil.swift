8c25805cc291b8c9e7ba5e10948b7318b2c19866
raghavbobal1 <Shivji7&>
2020-02-21 05:14:27 -0500

    [Raghav Bobal] commented everything to do later with a better approach.

--- RenterProjectW2020MAD3004/Util/PasswordUtil.swift
+++ RenterProjectW2020MAD3004/Util/PasswordUtil.swift
@@ -9,39 +9,44 @@
 import Foundation
 import CryptoKit
 
-class passwordUtil
-    
-{
-/* Function to generate the encryption key using User entered password*/
 
-func getSalt(withPassword userPassword:String) throws -> String
-{
-    let randomData = RNCryptor.randomData(ofLength: 32)
-    let cipherData = RNCryptor.encrypt(data: randomData, withPassword: userPassword)
-    return cipherData.base64EncodedString()
-}
+//TO DO
 
-/* Function to generated a secure password using
- genereated encryption key and user password*/
 
-func genereateSecurePassword(userPassword: String, encryptKey: String) throws -> String
-{
-    let encryptedKey = try getSalt(withPassword: encryptKey)
-    let securePassword = userPassword.data(using: .utf8)!
-    let cipherData = RNCryptor.encrypt(data: securePassword, withPassword: encryptedKey)
-    return cipherData.base64EncodedString()
-}
 
-/* Function to decrypt the password if user changes password,
- simply decrypt the key with the old password and re-encrypt it with the new password.*/
-
-func decryptPassword(securePassword: String, encryptKey: String) throws -> String
-        {
-
-        let encryptedPassword = Data.init(base64Encoded: securePassword)!
-        let decryptedData = try RNCryptor.decrypt(data: encryptedPassword, withPassword: encryptKey)
-        let decryptedString = String(data: decryptedData, encoding: .utf8)!
-
-        return decryptedString
-        }
-}
+//class passwordUtil
+//
+//{
+///* Function to generate the encryption key using User entered password*/
+//
+//func getSalt(withPassword userPassword:String) throws -> String
+//{
+//    let randomData = RNCryptor.randomData(ofLength: 32)
+//    let cipherData = RNCryptor.encrypt(data: randomData, withPassword: userPassword)
+//    return cipherData.base64EncodedString()
+//}
+//
+///* Function to generated a secure password using
+// genereated encryption key and user password*/
+//
+//func genereateSecurePassword(userPassword: String, encryptKey: String) throws -> String
+//{
+//    let encryptedKey = try getSalt(withPassword: encryptKey)
+//    let securePassword = userPassword.data(using: .utf8)!
+//    let cipherData = RNCryptor.encrypt(data: securePassword, withPassword: encryptedKey)
+//    return cipherData.base64EncodedString()
+//}
+//
+///* Function to decrypt the password if user changes password,
+// simply decrypt the key with the old password and re-encrypt it with the new password.*/
+//
+//func decryptPassword(securePassword: String, encryptKey: String) throws -> String
+//        {
+//
+//        let encryptedPassword = Data.init(base64Encoded: securePassword)!
+//        let decryptedData = try RNCryptor.decrypt(data: encryptedPassword, withPassword: encryptKey)
+//        let decryptedString = String(data: decryptedData, encoding: .utf8)!
+//
+//        return decryptedString
+//        }
+//}
