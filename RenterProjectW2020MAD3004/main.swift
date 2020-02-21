35129b33f313b599706a5bf8af30f4a87297f174
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-20 19:49:17 -0500

    [Abhishek]Read json data for customer

--- RenterProjectW2020MAD3004/main.swift
+++ RenterProjectW2020MAD3004/main.swift
@@ -12,24 +12,23 @@ func readJSON(fileName: String) -> [[String: Any]]?
 {
     let filepPath = Bundle.main.url(forResource: fileName, withExtension: "json")
     
-    guard let path = filepPath else {
+        guard let path = filepPath else {
         print("Invalid File path found")
         return nil
     }
     
-    guard let data = try? Data(contentsOf: path) else {
+        guard let data = try? Data(contentsOf: path) else {
         print("Error while read Data from URL")
         return nil
     }
     
-    guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
         print("Error while reading JSON Data from file")
         return nil
     }
     
     if let objectDictionary = json as? [[String: Any]]
         {
-            print(objectDictionary)
             return objectDictionary
         }
     else
@@ -39,3 +38,5 @@ func readJSON(fileName: String) -> [[String: Any]]?
         }
 }
 
+var c = readJSON(fileName:"CustomerData")
+print(c)
