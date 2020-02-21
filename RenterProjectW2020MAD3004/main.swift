c6043f05b80d7c5f4f79efb1cc5e8a1594223a3d
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-20 19:38:23 -0500

    [Abhishek]Added method to read json files

--- RenterProjectW2020MAD3004/main.swift
+++ RenterProjectW2020MAD3004/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, WorlnjbndAFKLJ.bmAKS!")
+func readJSON(fileName: String)
+{
+    let filepPath = Bundle.main.url(forResource: fileName, withExtension: "json")
+    
+    guard let path = filepPath else {
+        print("Invalid File path found")
+        return
+    }
+    
+    guard let data = try? Data(contentsOf: path) else {
+        print("Error while read Data from URL")
+        return
+    }
+    
+    guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+        print("Error while reading JSON Data from file")
+        return
+    }
+    
+    //print(json)
+    
+    if let userDictionary = json as? [String: Any]
+    {
+        //print(userDictionary)
+        let id = userDictionary["id"] ?? "No ID Found"
+        print(id)
+        //Like wise fetch all other values except address and company as it's another dictionary
+        if let addressDictionary = userDictionary["address"] as? [String: Any]
+        {
+            print(addressDictionary["city"]!)
+            
+        }
+        
+    }
 
