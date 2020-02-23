a31f36b78462977d963115397a6e1c0ad4ccc0d1
raghavbobal1 <Shivji7&>
2020-02-22 23:34:47 -0500

    [Raghav Bobal] moved email validation function to contact class and used it in init

--- RenterProjectW2020MAD3004/Person/Contact.swift
+++ RenterProjectW2020MAD3004/Person/Contact.swift
@@ -15,11 +15,48 @@ public class Contact
     private var emailId: String
     private var address: Address
     
+    
+    /* Function to validate the email id */
+    
+    public func isValid(emailAddressString: String) -> Bool
+     {
+         var returnValue = true
+         let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+    do
+     {
+         let regex = try NSRegularExpression(pattern: emailRegEx)
+         let nsString = emailAddressString as NSString
+         let results = regex.matches(in: emailAddressString, range: NSRange(location: 0, length: nsString.length))
+         
+         if results.count == 0
+         {
+             returnValue = false
+         }
+     }
+     catch let error as NSError
+     {
+         print("invalid regex: \(error.localizedDescription)")
+         returnValue = false
+     }
+     
+     return  returnValue
+     }
+
+    
     internal init(mobileNumber: String, emailId: String, address: Address)
        {
         self.mobileNumber = mobileNumber
-        self.emailId = emailId
         self.address = address
+        self.emailId = emailId
+        if isValid(emailAddressString: self.emailId)
+        {
+        self.emailId = emailId
+        }
+        else
+        {
+        print("Please enter a valid email address")
+        exit(0)
+        }
        }
     
     
