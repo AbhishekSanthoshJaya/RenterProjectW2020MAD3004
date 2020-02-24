91f1031ff666c33d602353375beb4ee8add9e525
prana <prakashrana9847067614@gmail.com>
2020-02-24 15:05:25 -0500

    [Prakash] moved person protocol to class

--- RenterProjectW2020MAD3004/Person/Person.swift
+++ RenterProjectW2020MAD3004/Person/Person.swift
@@ -18,21 +18,107 @@ class Person : IDisplayDelegate, TypeName
      var lastName: String
      var gender: Gender
      var birthDate: Date?
-     var age: Int
+     var age: Int = 0
      var userName: String
      var password: String
      var contact: Contact
     
-    init(id: String, firstName: String, lastName: String, gender: Gender,birthDate: Date?,age:Int, userName: String, password: String, contact: Contact) {
+    init(id: String, firstName: String, lastName: String, gender: Gender,birthDate: Date?, userName: String, password: String, contact: Contact) {
         self.id = id
         self.firstName = firstName
         self.lastName = lastName
         self.gender = gender
         self.birthDate = birthDate
-        self.age = age
         self.userName = userName
         self.password = password
         self.contact = contact
+    }
+    
+    init(personDict: [String: Any]) throws{
+        guard let id = personDict["id"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "id")
+        }
+        
+        
+        guard let firstName = personDict["firstName"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "firtName")
+        }
+        
+        guard let lastName = personDict["lastName"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "lastName")
+        }
+        
+        guard let genderString =  personDict["gender"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "gender")
+        }
+        let gender = Gender.getGenderType(genderString: genderString)
+        
+        
+        
+        guard let birthDateString = personDict["birthDate"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "birthDate")
+        }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        guard let birthDate = dateFormatter.date(from: birthDateString) else{
+            throw JsonValidationError.isNotValidInput(
+            className: String(describing:type(of: self)),
+            memberName: "birthDate")
+        }
+        
+        
+        guard let userName = personDict["userName"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "userName")
+        }
+        
+        
+        guard let password = personDict["password"] as? String else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "password")
+        }
+        
+        guard let addressJson = personDict["address"] as? [String: String] else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "address")
+        }
+        
+        guard let contactJson = personDict["contact"] as? [String: String] else {
+            throw JsonValidationError.isNotValidInput(
+                className: String(describing:type(of: self)),
+                memberName: "contact")
+        }
+        
+        var contact:Contact?
+        do{
+            var address:Address?
+            address = try Address(addressDict: addressJson)
+            contact = try Contact(contactDict: contactJson, address:address!)
+        }
+        
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.birthDate = birthDate
+        self.userName = userName
+        self.password = password
+        self.contact = contact!
+        
+        
     }
 }
 
