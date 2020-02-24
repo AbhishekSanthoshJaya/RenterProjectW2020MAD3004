f563e0606a6d1332599883e8c8e11b463faa5c20
prana <prakashrana9847067614@gmail.com>
2020-02-24 10:08:53 -0500

    [Prakash] Added validation struct for email and phone

--- RenterProjectW2020MAD3004/Exceptions/Validations.swift
+++ RenterProjectW2020MAD3004/Exceptions/Validations.swift
@@ -29,3 +29,38 @@ public enum CustomErrors: Error
 {
   case invalidEmail, invalidPhone
 }
+
+
+struct Validations {
+    private static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    static func email(email: String) throws {
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        if !emailPred.evaluate(with: email){
+            throw EmailValidationError.isNotValidEmail
+        }
+        
+        if email.isEmpty{
+            throw EmailValidationError.isEmpty
+        }
+        
+        if email.count > 254
+        {
+            throw EmailValidationError.isNotValidLength
+        }
+    }
+    
+    
+    static func mobileNumber(number: String) throws {
+        if number.count < 7 {
+            throw PhoneNumberValidationError.voiletsMinLength
+        }
+        
+        if number.count > 16 {
+            throw PhoneNumberValidationError.voiletsMaxLength
+        }
+    }
+    
+    
+}
