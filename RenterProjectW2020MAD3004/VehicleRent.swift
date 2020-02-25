1e2847306780e106910946ebe50dde4aebbbbbd0
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-24 19:30:57 -0500

    [Abhishek]Fixed bill calculation formula and added description in print

--- RenterProjectW2020MAD3004/VehicleRent.swift
+++ RenterProjectW2020MAD3004/VehicleRent.swift
@@ -38,13 +38,13 @@ class VehicleRent: IDisplayDelegate, TypeName
         print("End Date                : \(rentEndDate.printFormat())", to: &logger)
         print("Number Of Days          : \(numberOfDays)", to: &logger)
         print("KM Driven               : \(kmDriven.distanceFormat())", to: &logger)
-        print("Total Bill              : \(totalBill.priceFormat())", to: &logger)
+        print("Total Bill              : \(totalBill.priceFormat())  (Base Rate:\(vehicle.baseRate) * Number Of Days:\(numberOfDays)) + (Rate/KM:\(vehicle.perKmRate) * KM Driven:\(kmDriven))", to: &logger)
     }
     
     func calculateTotalBill() ->  Float{
         let baseRate = self.vehicle.baseRate
         let perKmRate = self.vehicle.perKmRate
-        return baseRate + (perKmRate * Float(numberOfDays))
+        return (baseRate * Float(numberOfDays)) + (perKmRate * Float(kmDriven))
     }
     
     func isLive() -> Bool{
